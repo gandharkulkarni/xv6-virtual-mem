12e56fe6b78f5b64cbcbec311dad59b9207a98d2
gandharkulkarni <gakulkarni@dons.usfca.edu>
2024-05-11 12:04:03 -0700

    Added kpages, udirs, kdirs, smem

--- xv6-riscv/kernel/defs.h
+++ xv6-riscv/kernel/defs.h
@@ -63,6 +63,7 @@ void            ramdiskrw(struct buf*);
 void*           kalloc(void);
 void            kfree(void *);
 void            kinit(void);
+uint64          kpages(void);
 
 // log.c
 void            initlog(int, struct superblock*);
@@ -106,6 +107,8 @@ void            yield(void);
 int             either_copyout(int user_dst, uint64 dst, void *src, uint64 len);
 int             either_copyin(void *dst, int user_src, uint64 src, uint64 len);
 void            procdump(void);
+uint64          smem(char *, int);
+pagetable_t     get_pagetable(struct proc *);
 
 // swtch.S
 void            swtch(struct context*, struct context*);
@@ -173,6 +176,8 @@ uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
+uint64          kdirs(void);
+uint64          udirs(void);
 
 // plic.c
 void            plicinit(void);
